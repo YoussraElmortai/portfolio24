66e698c3e1ed2efd60a1f71bd9e439d3eaa6760e
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2024-09-09 12:46:03 +0200

    index styling :lollipop:

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,33 @@
 export default function Home() {
   return (
     <>
+    <div className="circle"/>
       <header>
-        {/* about projects contact */}
+        <nav>
+          <ul>
+            <li>
+              <a href="">about</a>
+            </li>
+            <li>
+              <a href="">projects</a>
+            </li>
+            <li>
+              <a href="">contact</a>
+            </li>
+          </ul>
+        </nav>
       </header>
       <main>
         <h1>youssra Elmortai</h1>
         <p>Frontend developer and <span className="accent">Designer</span></p>
       </main>
+
       <footer>
-        {/* lang change and socials plus linkedin */}
+        <ul>
+          <li><a href=""><img src="linkedin.svg"></img></a></li>
+          <li><a href=""><img src="github.svg"></img></a></li>
+          <li><a href=""><img src="instagram.svg"></img></a></li>
+        </ul>
       </footer>
     </>
 
