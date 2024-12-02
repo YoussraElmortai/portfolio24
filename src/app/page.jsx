e5218e24313c1dcc99b04f55f1b064497836e243
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2024-12-02 16:25:31 +0100

    project scraped

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -24,31 +24,10 @@ export default function Home() {
           </ul>
         </section> */}
 
-        <section className="projects">
-          <div className="project">
-            <img src="https://i.pinimg.com/736x/85/ab/9b/85ab9b4233a63be0ada9281f6d179cfa.jpg" alt="project img"/>
+      
             <div className="tag"><span>Development</span><span>Design</span></div>
             <a className="link" href="">project name </a>
-          </div>
 
-          <div className="project">
-            <img src="https://i.pinimg.com/736x/3d/68/ec/3d68ec0adde30caab391fbbe6682f58e.jpg" alt="project img"/>
-            <div className="tag"><span>Development</span><span>Design</span></div>
-            <a className="link" href="">project name </a>
-          </div>
-
-          <div className="project">
-            <img src="https://i.pinimg.com/736x/08/5b/a2/085ba2e4b164e18d37d798ec7de8b85b.jpg" alt="project img"/>
-            <div className="tag"><span>Development</span><span>Design</span></div>
-            <a className="link" href="">project name </a>
-          </div>
-
-          <div className="project">
-            {/* <img src="https://i.pinimg.com/736x/08/5b/a2/085ba2e4b164e18d37d798ec7de8b85b.jpg" alt="project img"/>
-            <div className="tag"><span>Development</span><span>Design</span></div>
-            <a className="link" href="">project name </a> */}
-          </div>
-        </section>
       </main>
       <Footer />
     </>
