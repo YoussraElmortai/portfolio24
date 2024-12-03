c9d69bad3ce1aa77a33c92ab7489a511bd1ef0b3
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2024-12-03 12:04:54 +0100

    assets styling 💓

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,11 +9,15 @@ export default function Home() {
       <Header />
 
       <main>
-        <section className="intro">
-        <p>Frontend developer and <span className="accent">Designer</span> based in Amsterdam</p>
+        <section className='intro'>
+          <p>
+            Frontend developer and{" "}
+            <span className='accent'>Designer</span> based in
+            Amsterdam
+          </p>
         </section>
 
-        {/* <section className="skills">
+        <section className="skills">
           <ul>
             <li>html</li>
             <li>Javascript</li>
@@ -22,39 +26,97 @@ export default function Home() {
             <li>Figma</li>
             <li>Responsive design</li>
           </ul>
-        </section> */}
+        </section>
 
-<section className="wrapper">
-  <div className="card">
-    <img className="bgimage" src="https://i.pinimg.com/736x/af/5c/2d/af5c2da6d7e6a35e70e0c62cd4e78c8d.jpg" alt=""/>
-  <div className="tag"><span>Development</span><span>Design</span></div>
-  <a className="link" href="">project name <img src="arrow.png"/></a>
-  </div>
-  <div className="card">
-    <img className="bgimage" src="https://i.pinimg.com/736x/99/d0/da/99d0da79b955a30ab808b9f5662e01d7.jpg"  alt=""/>
-  <div className="tag"><span>Development</span><span>Design</span></div>
-  <a className="link" href="">project name<img src="arrow.png"/> </a>
-  </div>
-  <div className="card">
-  <img className="bgimage" src="https://i.pinimg.com/736x/af/5c/2d/af5c2da6d7e6a35e70e0c62cd4e78c8d.jpg" alt=""/>
-  <div className="tag"><span>Development</span><span>Design</span></div>
-  <a className="link" href="">project name <img src="arrow.png"/></a>
-  </div>
-  <div className="card">
-  <img className="bgimage" src="https://i.pinimg.com/736x/99/d0/da/99d0da79b955a30ab808b9f5662e01d7.jpg"  alt=""/>
-  <div className="tag"><span>Development</span><span>Design</span></div>
-  <a className="link" href="">project name <img src="arrow.png"/></a>
-  </div>
-  <div className="card">
-  <img className="bgimage" src="https://i.pinimg.com/736x/af/5c/2d/af5c2da6d7e6a35e70e0c62cd4e78c8d.jpg" alt=""/>
-  <div className="tag"><span>Development</span><span>Design</span></div>
-  <a className="link" href="">project name<img src="arrow.png"/> </a>
-  </div>
-</section>
+        <section className='projects'>
+          <h2>Selected Works</h2>
+          <div className='text'>
+            <p>
+              Selected works from school projects to personal projects
+            </p>
+          </div>
+          <section className='wrapper'>
+            <div className='card'>
+              <img
+                className='bgimage'
+                src='https://i.pinimg.com/736x/af/5c/2d/af5c2da6d7e6a35e70e0c62cd4e78c8d.jpg'
+                alt=''
+              />
+              <div className='tag'>
+                <span>Development</span>
+                <span>Design</span>
+              </div>
+              <a className='link' href=''>
+                project name <img src='arrow.png' />
+              </a>
+            </div>
 
+            <div className='card'>
+              <img
+                className='bgimage'
+                src='https://i.pinimg.com/736x/99/d0/da/99d0da79b955a30ab808b9f5662e01d7.jpg'
+                alt=''
+              />
+              <div className='tag'>
+                <span>Development</span>
+                <span>Design</span>
+              </div>
+              <a className='link' href=''>
+                project name
+                <img src='arrow.png' />{" "}
+              </a>
+            </div>
+
+            <div className='card'>
+              <img
+                className='bgimage'
+                src='https://i.pinimg.com/736x/af/5c/2d/af5c2da6d7e6a35e70e0c62cd4e78c8d.jpg'
+                alt=''
+              />
+              <div className='tag'>
+                <span>Development</span>
+                <span>Design</span>
+              </div>
+              <a className='link' href=''>
+                project name <img src='arrow.png' />
+              </a>
+            </div>
+
+            <div className='card'>
+              <img
+                className='bgimage'
+                src='https://i.pinimg.com/736x/99/d0/da/99d0da79b955a30ab808b9f5662e01d7.jpg'
+                alt=''
+              />
+              <div className='tag'>
+                <span>Development</span>
+                <span>Design</span>
+              </div>
+              <a className='link' href=''>
+                project name <img src='arrow.png' />
+              </a>
+            </div>
+
+            <div className='card'>
+              <img
+                className='bgimage'
+                src='https://i.pinimg.com/736x/af/5c/2d/af5c2da6d7e6a35e70e0c62cd4e78c8d.jpg'
+                alt=''
+              />
+              <div className='tag'>
+                <span>Development</span>
+                <span>Design</span>
+              </div>
+              <a className='link' href=''>
+                project name
+                <img src='arrow.png' />{" "}
+              </a>
+            </div>
+
+          </section>
+        </section>
       </main>
       <Footer />
     </>
-
   );
 }
