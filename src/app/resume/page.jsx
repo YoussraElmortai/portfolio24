8f7ca13134c21044017183e4900018af5f0b0ebf
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2025-03-07 20:15:48 +0100

    grid styling and content added 👍

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -25,9 +25,6 @@ export default function resume() {
             <a href='#education'>Education</a>
           </li>
           <li>
-            <a href='#certifications'>Certifications</a>
-          </li>
-          <li>
             <a href='#expertise'>Expertise</a>
           </li>
         </ul>
@@ -35,114 +32,88 @@ export default function resume() {
 
       <hr />
 
-      <section className='resume_wrapper' id='experience'>
-        <h2>experience</h2>
+      <section className='resume_section' id='experience'>
+        <h2>Experience</h2>
 
-        <article>
-          <h3>frontend developer at Unc inc</h3>
+        <div className='content'>
+          <article>
+            <h3>frontend developer at Unc inc</h3>
 
-          <div className='time'>
-            <time dateTime='2024-02'>February 2024</time> —{" "}
-            <time dateTime='2024-06'>June 2024</time>
-          </div>
+            <div className='time'>
+              <time dateTime='2024-02'>February 2024</time> —{" "}
+              <time dateTime='2024-06'>June 2024</time>
+            </div>
 
-          <p>
-            I contributed to the development and enhancement of
-            frontend components for the Unc Inc corporate website,
-            playing a key role in improving its functionality and user
-            experience. I also expanded and refined the existing
-            component library, ensuring greater consistency and
-            efficiency across projects. Using Docker, I managed and
-            modified Drupal fields to meet client needs effectively.
-            Additionally, I identified and resolved bugs on various
-            client websites, significantly improving their
-            accessibility, performance, and overall user satisfaction.
-          </p>
-        </article>
+            <p>
+              As a Frontend developer intern I contributed to the
+              development and enhancement of frontend components for
+              the Unc Inc corporate website, playing a key role in
+              improving its functionality and user experience. I also
+              expanded and refined the existing component library,
+              ensuring greater consistency and efficiency across
+              projects. Using Docker, I managed and modified Drupal
+              fields to meet client needs effectively. Additionally, I
+              identified and resolved bugs on various client websites,
+              significantly improving their accessibility,
+              performance, and overall user satisfaction.
+            </p>
+          </article>
+        </div>
       </section>
 
       <hr />
 
-      <section className='resume_wrapper' id='education'>
-        <h2>education</h2>
+      <section className='resume_section' id='education'>
+        <h2>Education</h2>
 
-        <article>
-          <h3>frontend developer at Unc inc</h3>
+        <div className='content'>
+          <article>
+            <h3>Amsterdam University of Applied Sciences</h3>
+            <h4>
+              associate bachelor degree in Frontend development and
+              design
+            </h4>
+            <div className='time'>
+              <time dateTime='2022-03'>September 2022</time> —{" "}
+              <time dateTime='2024-07'>June 2024</time>
+            </div>
 
-          <div className='time'>
-            <time dateTime='2024-02'>February 2024</time> —{" "}
-            <time dateTime='2024-06'>June 2024</time>
-          </div>
-
-          <p>
-            I contributed to the development and enhancement of
-            frontend components for the Unc Inc corporate website,
-            playing a key role in improving its functionality and user
-            experience. I also expanded and refined the existing
-            component library, ensuring greater consistency and
-            efficiency across projects. Using Docker, I managed and
-            modified Drupal fields to meet client needs effectively.
-            Additionally, I identified and resolved bugs on various
-            client websites, significantly improving their
-            accessibility, performance, and overall user satisfaction.
-          </p>
-        </article>
+            <p>
+              I majored in Frontend Development and Design, where I
+              gained hands-on experience by collaborating with local
+              agencies and businesses on small projects each semester.
+            </p>
+          </article>
+        </div>
       </section>
 
       <hr />
 
-      <section className='resume_wrapper' id='certifications'>
-        <h2>experience</h2>
+      <section className='resume_section' id='expertise'>
+        <h2>Expertise</h2>
 
-        <article>
-          <h3>frontend developer at Unc inc</h3>
+        <div className='content'>
+          <article>
+            <h3>HTML</h3>
+            <ul>
+              <li>HTML</li>
+            </ul>
+          </article>
 
-          <div className='time'>
-            <time dateTime='2024-02'>February 2024</time> —{" "}
-            <time dateTime='2024-06'>June 2024</time>
-          </div>
+          <article>
+            <h3>HTML</h3>
+            <ul>
+              <li>HTML</li>
+            </ul>
+          </article>
 
-          <p>
-            I contributed to the development and enhancement of
-            frontend components for the Unc Inc corporate website,
-            playing a key role in improving its functionality and user
-            experience. I also expanded and refined the existing
-            component library, ensuring greater consistency and
-            efficiency across projects. Using Docker, I managed and
-            modified Drupal fields to meet client needs effectively.
-            Additionally, I identified and resolved bugs on various
-            client websites, significantly improving their
-            accessibility, performance, and overall user satisfaction.
-          </p>
-        </article>
-      </section>
-
-      <hr />
-
-      <section className='resume_wrapper' id='expertise'>
-        <h2>experience</h2>
-
-        <article>
-          <h3>frontend developer at Unc inc</h3>
-
-          <div className='time'>
-            <time dateTime='2024-02'>February 2024</time> —{" "}
-            <time dateTime='2024-06'>June 2024</time>
-          </div>
-
-          <p>
-            I contributed to the development and enhancement of
-            frontend components for the Unc Inc corporate website,
-            playing a key role in improving its functionality and user
-            experience. I also expanded and refined the existing
-            component library, ensuring greater consistency and
-            efficiency across projects. Using Docker, I managed and
-            modified Drupal fields to meet client needs effectively.
-            Additionally, I identified and resolved bugs on various
-            client websites, significantly improving their
-            accessibility, performance, and overall user satisfaction.
-          </p>
-        </article>
+          <article>
+            <h3>HTML</h3>
+            <ul>
+              <li>HTML</li>
+            </ul>
+          </article>
+        </div>
       </section>
     </>
   );
