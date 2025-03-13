1e595ee98e4cff25ab3d95b9cd9789f04ef3301c
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2025-03-13 16:13:57 +0100

    routing fix ? ☁️

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -12,10 +12,10 @@ export default function Header() {
               </a>
             </li>
             <li>
-              <a href='/projects'>projects</a>
+              <a href='projects'>projects</a>
             </li>
             <li>
-              <a href='/about'>about</a>
+              <a href='about'>about</a>
             </li>
           </ul>
         </nav>
