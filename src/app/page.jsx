4adf33556610c74c3c85e7c7c96efcbe2e776800
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2025-06-01 21:14:40 +0200

    img

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,12 @@
 "use client";
 
 export default function Home() {
-  const date = new Date();
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
-
-  month = String(month).padStart(2, "0");
-  day = String(day).padStart(2, "0");
-
-  const starDate = `${day}.${month}.${year}`;
 
   return (
     <>
       <section className='landing'>
 <h1>Youssra Elmortai</h1>
+
       </section>
       <section className="carousel">
         <h2>Recent works</h2>
