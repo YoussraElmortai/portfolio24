a4064c031fac652d043ce4af374360753050cfe4
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2025-06-07 22:56:32 +0200

    component

---
+++ src/app/components/scroller/scroller.jsx
@@ -0,0 +1,76 @@
+"use client";
+import { useEffect } from "react";
+
+export default function Scroller() {
+      useEffect(() => {
+    const scrollers = document.querySelectorAll(".scroller");
+
+    const addAnimation = () => {
+      scrollers.forEach((scroller) => {
+        scroller.setAttribute("data-animated", true);
+
+        const scrollerInner = scroller.querySelector(
+          ".scroller__inner"
+        );
+        const scrollerContent = Array.from(scrollerInner.children);
+
+  
+        for (let i = 0; i < 3; i++) {
+          scrollerContent.forEach((item) => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute("aria-hidden", true);
+            scrollerInner.appendChild(duplicatedItem);
+          });
+        }
+      });
+    };
+
+    if (
+      !window.matchMedia("(prefers-reduced-motion: reduce)").matches
+    ) {
+      addAnimation();
+    }
+  });
+
+  return (
+    <>
+      <div className='scroller'>
+        <ul className='skill-list scroller__inner'>
+          <li>
+            <img src='/html.svg' />
+          </li>
+          <li>
+            <img src='/css.svg' />
+          </li>
+          <li>
+            <img src='/javascript.svg' />
+          </li>
+          <li>
+            <img src='/sass.svg' />
+          </li>
+          <li>
+            <img src='/svelte.svg' />
+          </li>
+          <li>
+            <img src='/react.svg' />
+          </li>
+          <li>
+            <img src='/next.svg' />
+          </li>
+          <li>
+            <img src='/vue.svg' />
+          </li>
+          <li>
+            <img src='/figma.svg' />
+          </li>
+          <li>
+            <img src='/bootstrap.svg' />
+          </li>
+          <li>
+            <img src='/vscode.svg' />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,36 +1,8 @@
 "use client";
-import { useEffect } from "react";
+
+import Scroller from "./components/scroller/scroller";
 
 export default function Home() {
-  useEffect(() => {
-    const scrollers = document.querySelectorAll(".scroller");
-
-    const addAnimation = () => {
-      scrollers.forEach((scroller) => {
-        scroller.setAttribute("data-animated", true);
-
-        const scrollerInner = scroller.querySelector(
-          ".scroller__inner"
-        );
-        const scrollerContent = Array.from(scrollerInner.children);
-
-  
-        for (let i = 0; i < 3; i++) {
-          scrollerContent.forEach((item) => {
-            const duplicatedItem = item.cloneNode(true);
-            duplicatedItem.setAttribute("aria-hidden", true);
-            scrollerInner.appendChild(duplicatedItem);
-          });
-        }
-      });
-    };
-
-    if (
-      !window.matchMedia("(prefers-reduced-motion: reduce)").matches
-    ) {
-      addAnimation();
-    }
-  });
 
   return (
     <>
@@ -39,43 +11,7 @@ export default function Home() {
           <h1>Youssra Elmortai</h1>
         </div>
         <div className='skills'>
-          <div className='scroller'>
-            <ul className='skill-list scroller__inner'>
-              <li>
-                <img src='/html.svg' />
-              </li>
-              <li>
-                <img src='/css.svg' />
-              </li>
-              <li>
-                <img src='/javascript.svg' />
-              </li>
-              <li>
-                <img src='/sass.svg' />
-              </li>
-              <li>
-                <img src='/svelte.svg' />
-              </li>
-              <li>
-                <img src='/react.svg' />
-              </li>
-              <li>
-                <img src='/next.svg' />
-              </li>
-              <li>
-                <img src='/vue.svg' />
-              </li>
-              <li>
-                <img src='/figma.svg' />
-              </li>
-              <li>
-                <img src='/bootstrap.svg' />
-              </li>
-              <li>
-                <img src='/vscode.svg' />
-              </li>
-            </ul>
-          </div>
+          <Scroller/>
         </div>
       </section>
 
